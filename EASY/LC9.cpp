b67690329863db664fb3d4fe443c012b151528ab
Ragav <sriragavandhandapani@gmail.com>
2026-02-06 23:45:21 +0530

    Added comments how the code works

--- EASY/LC9.cpp
+++ EASY/LC9.cpp
@@ -9,9 +9,9 @@ public:
         int temp = x;
         while(x>0)
         {
-            r = x%10;
-            rev = (rev*10) + r;
-            x /=10;
+            r = x%10;  //ex: 121%10 =>1  --> x will become 12 so   12%10=> 2  --> x will be 1 so 1%10=> 1
+            rev = (rev*10) + r; //0*10+0=>0  --> rev will become 1 so  1*10+0 =>10  --> rev will become 1*10+1=> <"101">
+            x /=10;   //121/10 =>12  --> 12/10=> 1  --> 1/10 => 1  
         }
         if(temp==rev)
         {
